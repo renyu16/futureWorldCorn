f8099e67ee602d8df654589d2663eeae0132de3f
renzong <rztiaogao@126.com>
2026-07-10 17:57:07 +0800

    feat: add TokenHouse OZ Governor wrapper

---
+++ src/TokenHouse.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+import "@openzeppelin/contracts/governance/Governor.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorSettings.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorTimelockControl.sol";
+
+contract TokenHouse is
+    Governor,
+    GovernorSettings,
+    GovernorCountingSimple,
+    GovernorVotes,
+    GovernorVotesQuorumFraction,
+    GovernorTimelockControl
+{
+    constructor(IVotes _govCORN, TimelockController _timelock)
+        Governor("TokenHouse")
+        GovernorSettings(1, 50400, 0)
+        GovernorVotes(_govCORN)
+        GovernorVotesQuorumFraction(4)
+        GovernorTimelockControl(_timelock)
+    {}
+
+    function votingDelay() public view override(Governor, GovernorSettings) returns (uint256) {
+        return GovernorSettings.votingDelay();
+    }
+
+    function votingPeriod() public view override(Governor, GovernorSettings) returns (uint256) {
+        return GovernorSettings.votingPeriod();
+    }
+
+    function proposalThreshold() public view override(Governor, GovernorSettings) returns (uint256) {
+        return GovernorSettings.proposalThreshold();
+    }
+
+    function quorum(uint256 blockNumber)
+        public view override(Governor, GovernorVotesQuorumFraction) returns (uint256)
+    {
+        return GovernorVotesQuorumFraction.quorum(blockNumber);
+    }
+
+    function state(uint256 proposalId)
+        public view override(Governor, GovernorTimelockControl) returns (ProposalState)
+    {
+        return GovernorTimelockControl.state(proposalId);
+    }
+
+    function proposalNeedsQueuing(uint256 proposalId)
+        public view override(Governor, GovernorTimelockControl) returns (bool)
+    {
+        return GovernorTimelockControl.proposalNeedsQueuing(proposalId);
+    }
+
+    function _queueOperations(uint256 proposalId, address[] memory targets, uint256[] memory values, bytes[] memory calldatas, bytes32 descriptionHash)
+        internal override(Governor, GovernorTimelockControl) returns (uint48)
+    {
+        return GovernorTimelockControl._queueOperations(proposalId, targets, values, calldatas, descriptionHash);
+    }
+
+    function _executeOperations(uint256 proposalId, address[] memory targets, uint256[] memory values, bytes[] memory calldatas, bytes32 descriptionHash)
+        internal override(Governor, GovernorTimelockControl)
+    {
+        GovernorTimelockControl._executeOperations(proposalId, targets, values, calldatas, descriptionHash);
+    }
+
+    function _cancel(address[] memory targets, uint256[] memory values, bytes[] memory calldatas, bytes32 descriptionHash)
+        internal override(Governor, GovernorTimelockControl) returns (uint256)
+    {
+        return GovernorTimelockControl._cancel(targets, values, calldatas, descriptionHash);
+    }
+
+    function _executor()
+        internal view override(Governor, GovernorTimelockControl) returns (address)
+    {
+        return GovernorTimelockControl._executor();
+    }
+}

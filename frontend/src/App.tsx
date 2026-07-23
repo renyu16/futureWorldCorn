f6183e5c33b0b5735502ad74049b7f743d528bec
renzong <rztiaogao@126.com>
2026-07-23 14:49:36 +0800

    feat: add HumanHouse navigation to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import { CreateMarket } from './pages/CreateMarket'
 import { Portfolio } from './pages/Portfolio'
 import { Delegate } from './pages/Delegate'
 import { Governance } from './pages/Governance'
+import { HumanHouse } from './pages/HumanHouse'
 
 function App() {
   const [page, setPage] = useState('list')
@@ -21,6 +22,7 @@ function App() {
           <button onClick={() => setPage('portfolio')}>Portfolio</button>
           <button onClick={() => setPage('delegate')}>Delegate</button>
           <button onClick={() => setPage('governance')}>Governance</button>
+          <button onClick={() => setPage('humanhouse')}>Disputes</button>
         </nav>
         <WalletConnect />
       </header>
@@ -31,6 +33,7 @@ function App() {
         {page === 'portfolio' && <Portfolio />}
         {page === 'delegate' && <Delegate />}
         {page === 'governance' && <Governance />}
+        {page === 'humanhouse' && <HumanHouse />}
       </main>
     </>
   )
